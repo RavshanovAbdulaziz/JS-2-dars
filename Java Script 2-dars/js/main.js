6054c9d00ca67f27975d6a6a9c79a355758cfd26
RavshanovAbdulaziz <abdulazizravshanov28@gmail.com>
2023-11-08 19:06:23 +0500

    JS 2-dars
    27-guruh

---
+++ Java Script 2-dars/js/main.js
@@ -0,0 +1,75 @@
+/*
+ if (agar)
+ else if (yoki bolmasa)
+ else (u holda)
+*/
+
+/*
+if (44>='44'){
+    console.log("Ular teng!");
+}
+else {
+    console.log("Ular teng emas!")
+}*/
+
+/*if (45 == 44){
+    console.log("Ular teng!-1")
+}
+else if (45 == 46){
+    console.log("Ular teng!-2")
+}
+else{
+    console.log("Yuqoridagilarning hech biri teng emas!")
+}*/
+
+/*if (400 == 400){
+    console.log("O'rtacha nootebook olaman!")
+}
+else if (750 == 750){
+    console.log("Yaxshi notebook olaman!")
+}
+else{
+    console.log("Notebook olmayman!")
+}*/
+
+
+/*if ("Imtihondan-o'tsam" == "Imtihondan-o'tsam"){
+    console.log("O'qishga kiraman")
+}
+else if ("O'tmasam" == "O'tmasam"){
+    console.log("Boshqattan topshiraman!")
+}
+else{
+    console.log("Ishlayman!")
+}*/
+
+
+
+let a = 10;
+let b = 46;
+let c = 49;
+let d = 55;
+
+/*if (a == 11 && b == 46 && c == 49 && d == 55){
+    console.log("Solishtirilgan qiymatning hamasi tog'ri!")
+}
+else{
+    console.log("Solishtirilgan qiymatlarning kamida  bittasi notogri!")
+}*/
+
+/*if (a == 10 || b == 49 || c == 48 || d == 56){
+    console.log("Solishtirilgan qiymatlarning kamida bittasi tog'ri!")
+}
+else{
+    console.log("Solishtirilgan qiymatlarning hammasi notog'ri!")
+}*/
+
+/*Data types in JS: undefined,boolean*/
+/*let matn = "Java Script"*/ /*string*/
+/*let son = 45;*/            /*number*/
+/*let text;*/                /*undefined*/
+/*let rost = true;*/         /*boolean*/
+
+let rost = 45 == 45 ? true : false;
+
+document.write(rost);
